5553c4a2917b00bb4d383f48f805b9864cde9cf0
neilpeter08 <neilpeter_garcia08@yahoo.com>
2015-11-20 18:57:50 +0800

    added wallpapers

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,14 +2,27 @@
 <html>
 <head>
   <title></title>
-  <link rel="stylesheet" type="text/css" href="/css/style.css">
+  <link rel="stylesheet" type="text/css" href="/css/bootstrap-theme.css">
+  <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="/css/register-style.css">
+  <script src="/js/jquery.js"></script>
+  <script src="/js/bootstrap.min.js"></script>
 </head>
 <body>
-    {!! Form::open(array('class'=>'form-signup')) !!}
-    {!! Form::text('firstname', null, array('placeholder'=>'Enter your first name.', 'class'=>'btn text-signup')) !!}
-    {!! Form::text('lastname', null, array('placeholder'=>'Enter your last name.', 'class'=>'btn text-signup')) !!}
-    {!! Form::text('studentnumber', null, array('placeholder'=>'Enter your student number.', 'class'=>'btn text-signup')) !!}
-    {!! Form::submit('Signup', array('class'=>'btn btn-signup')) !!}
-    {!! Form::close() !!}
+    @include('layouts.header');
+    <div class="content">
+      {!! Form::open(array('class'=>'form-signup')) !!}
+      {!! Form::text('firstname', null, array('placeholder'=>'Enter your first name.', 'class'=>'txt text-signup')) !!}
+      {!! Form::text('lastname', null, array('placeholder'=>'Enter your last name.', 'class'=>'txt text-signup')) !!}
+      {!! Form::text('studentnumber', null, array('placeholder'=>'Enter your student number.', 'class'=>'txt text-signup')) !!}
+      {!! Form::submit('Sign me up!', array('class'=>'txt btn-signup')) !!}
+      {!! Form::close() !!}
+
+    <div class="message">
+        <h1>Personal Instructing Agent</h1>
+        <h2>An intelligent tutoring system built for students learning linear equations. Register now and be a part of the group!</h2>
+    </div>
+    </div>
+
 </body>
 </html>
