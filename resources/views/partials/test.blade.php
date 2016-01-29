d979395d639de154a8eefd61403ae362fa26758d
neil <neilpeter_garcia08@yahoo.com>
2016-01-30 01:14:48 +0800

    bug fix

--- resources/views/partials/test.blade.php
+++ resources/views/partials/test.blade.php
@@ -1,9 +1,9 @@
 <?php
 
-//   $file="demo.xls";
-// $test="<table  ><tr><td>Cell 1</td><td>Cell 2</td></tr></table>";
-// header("Content-type: application/vnd.ms-excel");
-// header("Content-Disposition: attachment; filename=$file");
+  $file="demo.xls";
+$test="<table  ><tr><td>Cell 1</td><td>Cell 2</td></tr></table>";
+header("Content-type: application/vnd.ms-excel");
+header("Content-Disposition: attachment; filename=$file");
 
 
 ?>
