5146368e0e196def852e8f8a692fca8e7ba4d685
neil <neilpeter_garcia08@yahoo.com>
2016-01-30 21:53:37 +0800

    excel download

--- resources/views/partials/test.blade.php
+++ resources/views/partials/test.blade.php
@@ -2,6 +2,11 @@
 
 use Carbon\Carbon;
 
+header("Content-Type:   application/vnd.ms-excel; charset=utf-8");
+header("Content-Disposition: attachment; filename=abc.xls");  //File name extension was wrong
+header("Expires: 0");
+header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+header("Cache-Control: private",false);
 
 
 ?>
